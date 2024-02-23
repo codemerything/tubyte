df9cc6fda0bc4ef1858fc8d02e75f72459612c59
mmnldm <mremmanueledem@gmail.com>
2024-02-23 23:52:06 +0100

    feat: rename jsx extension

--- app/components/Title.jsx
+++ app/components/Title.jsx
@@ -5,7 +5,7 @@ export default function Title() {
   return (
     <div className="inline-flex mt-[33px] ml-[66px] gap-2">
       <Image src="/icons/logo.svg" alt="Tubyte logo" height="37" width="90" />
-      <div className="bg-blue-light px-3 mt-1 h-6 flex font-medium rounded-lg font-poppins text-sm text-center items-center">
+      <div className="bg-blue-light px-3 mt-1 h-6 flex font-medium rounded-lg font-poppins text-sm text-center items-center text-white">
         Beta
       </div>
     </div>
